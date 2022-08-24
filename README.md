f52b30ce63bc68be0d162ab55acb513171a71268
nadeenay <70846138+nadeenay@users.noreply.github.com>
2022-08-24 05:57:53 +0200

    add read me file

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Image-Prossesing-API
+it's a simple image processing API used to resize images .
+
+## URL format
+```
+{baseUrl}/api/images/filename=imgName&width=requiredWidth&height=requiredHeight
+```
+### Steps to run the project 
+## Scripts to run 
+```
+1 - clone the project
+2 - npm install --> to install required packages.
+3 - npm run start --> to start run the server.
+4 Ctrl+C
+5 - npm run test --> to run the tests. 
+Note : you should press ctrl +c first before run tests to stop running the server as the test run the server again.
+```
--- src/routes/api/middlewares/validate.ts
+++ src/routes/api/middlewares/validate.ts
@@ -1,16 +1,34 @@
 import { Request, Response, NextFunction } from 'express';
+import path from 'path';
+import fs from 'fs';
 
 const validUrl = (
   req: Request,
   res: Response,
   next: NextFunction
 ): void | never => {
+
   const imgname: unknown = req.query.filename;
   const width: unknown = req.query.width;
   const height: unknown = req.query.height;
 
+  const fileInPath = path.join(
+    __dirname,
+    '..',
+    '..',
+    '..',
+    '..',
+    'public',
+    'images',
+    (imgname + '.jpg') as string
+  );
+
   if (imgname === undefined || width === undefined || height === undefined) {
     res.status(400).send('Wrong url');
-  } else next();
+  } else if (fs.existsSync(fileInPath)) {
+    next();
+  } else {
+    res.status(400).send('Image not exist');
+  }
 };
 export default validUrl;
